e6fb1f7403d951a82045ffefd6f259a4dbe45860
yanndupis <yann.dupis@gmail.com>
2022-06-10 18:15:51 -0700

    Fix typo

--- examples/main.py
+++ examples/main.py
@@ -2,6 +2,6 @@ from pycape.cape import Cape
 
 if __name__ == "__main__":
     cape = Cape()
-    input = 1
-    result = cape.run("8F467C2F-E507-45C5-B486-4E767C375E23", input)
-    print(f"The result is: result {result}")
+    input = 20
+    result = cape.run("c052214b-a597-42fc-9a2f-d48ea9dc9902", input)
+    print(f"The result is: {result}")
