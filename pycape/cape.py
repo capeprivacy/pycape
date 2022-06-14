020ce3310b710088243240a1472dc434e03d1033
Justin Patriquin <justinpatriquin@gmail.com>
2022-06-14 17:38:16 +0000

    update attestation document json to match sentinel
    Few other updates to make this runnable for me:
    
    - Update the example to be more generic
    - Add insecure flag to test against localhost without proper creds

--- pycape/cape.py
+++ pycape/cape.py
@@ -2,6 +2,7 @@ import asyncio
 import base64
 import json
 import random
+import ssl
 
 import websockets
 
@@ -10,9 +11,10 @@ from pycape.enclave_encrypt import encrypt
 
 
 class Cape:
-    def __init__(self):
-        self._url = "wss://cape.run"
-        self._auth_token = "not_implemented"
+    def __init__(self, url="wss://cape.run", token="", insecure=False):
+        self._url = url
+        self._auth_token = token
+        self._insecure = insecure
 
     def run(self, function_id, input):
         return asyncio.run(self._run(function_id, input))
@@ -20,17 +22,25 @@ class Cape:
     async def _run(self, function_id, input):
         endpoint = f"{self._url}/v1/run/{function_id}"
 
-        async with websockets.connect(endpoint) as websocket:
+        ctx = ssl.create_default_context()
 
+        if self._insecure:
+            ctx.check_hostname = False
+            ctx.verify_mode = ssl.CERT_NONE
+
+        async with websockets.connect(endpoint, ssl=ctx) as websocket:
             nonce = _generate_nonce()
             request = _create_request(self._auth_token, nonce)
             await websocket.send(request)
 
-            attestation = await websocket.recv()
-            public_key = parse_attestation(attestation)
+            msg = await websocket.recv()
+            attestation_doc = json.loads(msg)
+            doc = base64.b64decode(attestation_doc["message"])
+            public_key = parse_attestation(doc)
 
             input_bytes = _convert_input_to_bytes(input)
             ciphertext = encrypt(input_bytes, public_key)
+
             await websocket.send(ciphertext)
 
             result = await websocket.recv()
@@ -39,7 +49,7 @@ class Cape:
             return result
 
 
-# TODO What should be the lenght?
+# TODO What should be the length?
 def _generate_nonce(length=8):
     return "".join([str(random.randint(0, 9)) for i in range(length)])
 
