83472cd41247e0ced0b25c668bdb8e3483185276
Justin Patriquin <justinpatriquin@gmail.com>
2022-07-19 12:47:22 +0000

    verify attestation document signature

--- pycape/attestation_test.py
+++ pycape/attestation_test.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+import cbor2
+
 from pycape.attestation import parse_attestation
+from pycape.attestation import verify_signature
 
 
 class TestAttestation:
@@ -10,3 +13,13 @@ class TestAttestation:
             attestation = f.read()
         public_key = parse_attestation(attestation)
         assert len(public_key) == 32
+
+    def test_verify_signature(self):
+        fixture_dir = Path(__file__).parent / "fixtures"
+        with open(fixture_dir / "attestation_example_hpke.bin", "rb") as f:
+            attestation = f.read()
+
+        payload = cbor2.loads(attestation)
+        doc = cbor2.loads(payload[2])
+
+        verify_signature(attestation, doc["certificate"])
