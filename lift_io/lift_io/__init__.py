00a7fa65c7ca5a2b40a0703772595af8919f0170
jvmncs <jvmncs@gmail.com>
2022-07-29 12:12:51 -0400

    add packaging necessities for lift_io

--- lift_io/lift_io/__init__.py
+++ lift_io/lift_io/__init__.py
@@ -1,1 +1,9 @@
 from io_lifter import lift_io
+from serde import bundle_serde_hooks
+from serde import SerdeHookBundle
+
+__all__ = [
+    "lift_io",
+    "bundle_serde_hooks",
+    "SerdeHookBundle"
+]
