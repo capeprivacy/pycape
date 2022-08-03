2c83881de502f19e4864e3653ac2588d06fb3b24
jvmncs <jason@manc.us>
2022-08-03 13:56:04 -0400

    add docstrings to public pycape API

--- pycape/function_ref.py
+++ pycape/function_ref.py
@@ -1,5 +1,39 @@
+"""A structured representation of a deployed Cape function.
+
+A FunctionRef is intended to capture any/all metadata related to a Cape function.
+It is generally created from user-supplied metadata, which is given to the user
+as output of the Cape CLI's `deploy` command.
+
+Usage:
+    fid = "asdf231lkg1324afdg"
+    fhash = str(b"2l1h21jhgb2k1jh3".hex())
+    fref = FunctionRef(fid, fhash)
+
+    cape = Cape()
+    cape.connect(fref)
+
+    fref2 = FunctionRef(fid)
+    fref2.set_hash(fhash)
+
+    assert fref == fref2
+"""
+
+
 class FunctionRef:
+    """A structured reference to a Cape function."""
+
     def __init__(self, function_id, function_hash=None):
+        """Instantiate a FunctionRef.
+
+        Args:
+            function_id: Required string denoting the function ID of the deployed Cape
+                function. This is typically given in the output of the Cape CLI's
+                `deploy` command.
+            function_hash: Optional string denoting the function hash of the deployed
+                Cape function. If supplied, the Cape client will attempt to verify that
+                enclave responses include a matching function hash whenever this
+                FunctionRef is included in Cape requests.
+        """
         if function_id is None:
             raise ValueError("Function id was not provided.")
         self._function_id = function_id
