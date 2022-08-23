9cef9decd8a0225e0dc479e593cc56a263efe64e
ekloberdanz <kloberdanz.eliska@gmail.com>
2022-08-23 10:48:02 -0500

    format

--- pycape/tests/tests.py
+++ pycape/tests/tests.py
@@ -1,13 +1,17 @@
-import pycape
-from pycape import Cape
-from pycape.cape import _convert_to_function_ref, _generate_nonce, _create_connection_request
+import json
 import unittest
 from unittest.mock import Mock
-import json
+
+import pycape
+from pycape import Cape
+from pycape.cape import _convert_to_function_ref
+from pycape.cape import _create_connection_request
+from pycape.cape import _generate_nonce
+
 
 class TestCape(unittest.TestCase):
     def test_convert_to_function_ref(self):
-        CAPE_FUNCTION_ID = 'mHwsZ9Bh5cK4Bz8utHjdhy'
+        CAPE_FUNCTION_ID = "mHwsZ9Bh5cK4Bz8utHjdhy"
         fun_ref = _convert_to_function_ref(CAPE_FUNCTION_ID)
         self.assertTrue(isinstance(fun_ref, pycape.function_ref.FunctionRef))
 
@@ -16,19 +20,20 @@ class TestCape(unittest.TestCase):
         nonce = _generate_nonce(length=length)
         self.assertTrue(isinstance(nonce, str))
         self.assertTrue(len(nonce), length)
-    
+
     def test_create_connection_request(self):
-        nonce = '90444145'
+        nonce = "90444145"
         conn_req = _create_connection_request(nonce)
         self.assertEqual(conn_req, json.dumps({"message": {"nonce": "90444145"}}))
 
     def test_connect(self):
         Cape = Mock()
         client = Cape()
-        CAPE_FUNCTION_ID = 'mHwsZ9Bh5cK4Bz8utHjdhy'
+        CAPE_FUNCTION_ID = "mHwsZ9Bh5cK4Bz8utHjdhy"
         function_ref = _convert_to_function_ref(CAPE_FUNCTION_ID)
         client.connect(function_ref)
         client.connect.assert_called_with(function_ref)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
