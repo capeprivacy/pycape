8b74d1eb64cd450dc650d7550ee292c2aaeb7b91
jvmncs <jvmncs@gmail.com>
2022-08-24 14:21:23 -0400

    add failing test

--- pycape/enclave_encrypt_test.py
+++ pycape/enclave_encrypt_test.py
@@ -1,13 +1,14 @@
+import hybrid_pke
 from cose.keys.curves import P384
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.serialization import Encoding
 
+from pycape import enclave_encrypt
 from pycape.attestation import parse_attestation
 from pycape.attestation_test import create_attestation_doc
 from pycape.attestation_test import create_certs
 from pycape.attestation_test import create_cose_1_sign_msg
-from pycape import enclave_encrypt
 
 
 class TestEnclaveEncryption:
@@ -30,3 +31,29 @@ class TestEnclaveEncryption:
         ctx = enclave_encrypt.EncryptionContext(public_key)
         sealed = ctx.seal(plaintext)
         assert len(sealed) > 32  # encapsulated key is 32 bytes for default hpke config
+
+    def test_multiroundtrip(self):
+        plaintext = b"private_data"
+        hpke = hybrid_pke.default()
+
+        crv = P384
+        root_private_key = ec.generate_private_key(
+            crv.curve_obj, backend=default_backend()
+        )
+        private_key = ec.generate_private_key(crv.curve_obj, backend=default_backend())
+
+        root_cert, cert = create_certs(root_private_key, private_key)
+        doc_bytes = create_attestation_doc(root_cert, cert)
+        attestation = create_cose_1_sign_msg(doc_bytes, private_key)
+
+        public_key, _ = parse_attestation(
+            attestation, root_cert.public_bytes(Encoding.PEM)
+        )
+        ctx = enclave_encrypt.EncryptionContext(public_key)
+
+        for _ in range(3):
+            sealed = ctx.seal(plaintext)
+            encap = sealed[:32]
+            cipher = sealed[32:]
+            result = hpke.open(encap, cipher_txt=cipher, info=b"", aad=b"")
+            assert result == b"private_data"
