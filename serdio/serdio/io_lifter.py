d9e26a091cd9d4622dbd2e178a1c3f13174db443
jvmncs <jvmncs@gmail.com>
2022-09-02 14:58:23 -0400

    improve docstrings throughout pycape/serdio

--- serdio/serdio/io_lifter.py
+++ serdio/serdio/io_lifter.py
@@ -33,41 +33,47 @@ from serdio import serde
 def lift_io(
     f=None, *, encoder_hook=None, decoder_hook=None, hook_bundle=None, as_handler=False
 ):
-    """Lift a function into a callable that abstracts input-output (de-)serialization.
+    """Lift a function into an :class:`.IOLifter`.
 
-    The resulting callable is nearly identical to the original function,
-    however it can also easily be converted to a Cape handler with `as_cape_handler`.
-    The Cape handler expects msgpack-ed bytes as input and also msgpacks its output,
-    which conforms to the kinds of Python functions that can be `Cape.invoke`-ed.
+    The resulting :class:`.IOLifter` Callable is nearly identical to the original
+    function, however it can also easily be converted to a bytes-handler with
+    :meth:`~IOLifter.as_bytes_handler`. The bytes-handler expects Serdio bytes as input
+    and returns Serdio bytes as its output.
 
     This decorator expects at most one of these sets of kwargs to be specified:
-        - `encoder_hook` and `decoder_hook`
-        - `hook_bundle`
+        - ``encoder_hook`` and ``decoder_hook``
+        - ``hook_bundle``
 
     Args:
-        f: A Callable to be invoked or run with Cape.
+        f: A Callable to be IO-lifted into a bytes-handler.
         encoder_hook: An optional Callable that specifies how to convert custom-typed
-          inputs or outputs into msgpack-able Python types (e.g. converting
-          MyCustomClass into a dictionary of Python natives).
-        decoder_hook: An optional Callable that specifies how to invert encoder_hook
-          for custom-typed inputs and outputs.
-        hook_bundle: An optional tuple, list, or SerdeHookBundle that simply packages up
-          encoder_hook and decoder_hook Callables into a single object.
-        as_handler: A boolean controlling the return type of the decorator. If False,
-            returns an IOLifter wrapping up `f` and the hook bundle specified by the
-            combination of `encoder_hook`/`decoder_hook`/`hook_bundle`. If True, returns
-            the result of applying lambda x: x.as_cape_handler() to the IOLifter.
+            inputs or outputs into msgpack-able Python types (e.g. converting
+            MyCustomClass into a dictionary of Python natives). See
+            :class:`serdio.SerdeHookBundle` for details.
+        decoder_hook: An optional Callable that specifies how to invert ``encoder_hook``
+            for custom-typed inputs and outputs. See :class:`serdio.SerdeHookBundle` for
+            details.
+        hook_bundle: An optional tuple, list, or :class:`~serdio.SerdeHookBundle` that
+            simply packages up ``encoder_hook`` and ``decoder_hook`` Callables into a
+            single object.
+        as_handler: A boolean controlling the return type of the decorator. If
+            ``False``, returns an :class:`.IOLifter` wrapping up ``f`` and the hook
+            bundle specified by the supplied combination of ``encoder_hook``,
+            ``decoder_hook``, and ``hook_bundle``. If True, returns the result of
+            applying ``lambda x: x.as_bytes_handler()`` to the :class:`.IOLifter`.
 
     Returns:
-        An IOLifter wrapping up `f`, `encoder_hook`, and `decoder_hook` that can be
-        used in a deployable Cape script or can be run/invoked by the Cape client.
-        If as_handler=True, instead returns the IO-lifted version of `f`.
+        An :class:`.IOLifter` wrapping up ``f``, ``encoder_hook``, and ``decoder_hook``
+        with the machinery needed to convert arbitrary functions into bytes-handlers. If
+        ``as_handler=True``, returns the bytes-handler version of ``f``.
 
     Raises:
-        ValueError if wrong combination of encoder_hook, decoder_hook, hook_bundle is
-          supplied.
-        TypeError if hook_bundle is not coercible to SerdeHookBundle, or if
-          encoder_hook/decoder_hook are not Callables.
+        ValueError: if user supplies wrong combination of ``encoder_hook``,
+            ``decoder_hook``, and ``hook_bundle``.
+        TypeError: if ``hook_bundle`` is not coercible to
+            :class:`~serdio.SerdeHookBundle`, or if ``encoder_hook``/``decoder_hook``
+            are not Callables.
+
     """
     _check_lift_io_kwargs(encoder_hook, decoder_hook, hook_bundle)
     if encoder_hook is not None:
@@ -84,6 +90,14 @@ def lift_io(
 
 
 class IOLifter:
+    """A Callable for lifting arbitrary functions into equivalent bytes-handlers.
+
+    Args:
+        f: The function we want to lift.
+        hook_bundle: An optional :class:`~.serdio.serde.SerdeHookBundle` for dealing
+            with user-defined types
+    """
+
     def __init__(
         self,
         f: Callable,
@@ -96,9 +110,14 @@ class IOLifter:
         return self._func(*args, **kwargs)
 
     def as_cape_handler(self):
+        """Alias of :meth:`.IOLifter.as_bytes_handler`."""
         return self.as_bytes_handler()
 
     def as_bytes_handler(self):
+        """Lift the wrapped Callable into its functionally-equivalent bytes-handler.
+
+        A bytes-handler is a Callable mapping Serdio bytes to Serdio bytes.
+        """
         if self.hook_bundle is not None:
             encoder_hook, decoder_hook = self.hook_bundle.unbundle()
         else:
@@ -111,9 +130,9 @@ class IOLifter:
                 )
             except ValueError:
                 raise ValueError(
-                    "Couldn't deserialize the function's input with MessagePack."
-                    "Make sure your input is serialized with MessagePack manually or "
-                    "by setting use_serdio=True in Cape.run or Cape.invoke"
+                    "Couldn't deserialize the function's input with Serdio. "
+                    "Make sure your input is serialized according to the Serdio spec "
+                    "manually, or by setting use_serdio=True in Cape.run / Cape.invoke."
                 )
             _check_inputs_match_signature(self._func, args, kwargs)
             output = self._func(*args, **kwargs)
