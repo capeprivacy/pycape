6c38669d10b51096305dc6c8be526015b87db50d
jvmncs <jvmncs@gmail.com>
2022-09-13 13:59:29 -0400

    change deploy_run_echo example to use cape encrypt for input

--- examples/deploy_run_echo.py
+++ examples/deploy_run_echo.py
@@ -1,13 +1,14 @@
+import pathlib
 import subprocess
 
-from pycape import Cape
-from pycape import FunctionRef
+import pycape
 
 CAPE_HOST = "wss://enclave.capeprivacy.com"
+ECHO_DEPLOY_PATH = pathlib.Path(__file__).parent.absolute() / "echo"
 
 # Cape deploy function
 proc_deploy = subprocess.Popen(
-    "cape deploy ./echo --url " + CAPE_HOST,
+    f"cape deploy {ECHO_DEPLOY_PATH} --url {CAPE_HOST}",
     shell=True,
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
@@ -17,17 +18,21 @@ err_deploy = err_deploy.decode()
 
 # Parse stderr to get CAPE_FUNCTION_ID and CAPE_CHECKSUM
 err_deploy = err_deploy.split("\n")
+function_id = function_checksum = None
 for i in err_deploy:
     if "Function ID" in i:
-        function_id = i.split(" ")
-        CAPE_FUNCTION_ID = function_id[3]
-    elif "Checksum" in i:
-        checksum = i.split(" ")
-        CAPE_CHECKSUM = checksum[2]
+        id_output = i.split(" ")
+        function_id = id_output[3]
+    elif "Function Checksum" in i:
+        checksum_output = i.split(" ")
+        function_checksum = checksum_output[2]
+
+if function_id is None:
+    raise RuntimeError(f"Function ID not found in 'deploy' response: \n{err_deploy}")
 
 # Run function
-function_ref = FunctionRef(CAPE_FUNCTION_ID, CAPE_CHECKSUM)
-cape = Cape(url=CAPE_HOST)
-input = "Welcome to Cape".encode()
-result = cape.run(function_ref, input)
+function_ref = pycape.FunctionRef(function_id, function_checksum)
+cape = pycape.Cape(url=CAPE_HOST)
+message = cape.encrypt("Welcome to Cape".encode())
+result = cape.run(function_ref, message)
 print(f"The result is: {result.decode()}")
