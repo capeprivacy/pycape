fc346377a42ce56f042057c4b1e7fec3687ee11a
jvmncs <jvmncs@gmail.com>
2022-09-13 13:56:10 -0400

    fix cape_encrypt

--- pycape/cape_encrypt.py
+++ pycape/cape_encrypt.py
@@ -1,8 +1,8 @@
 import os
 from typing import Tuple
 
-from cryptography import x509
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.ciphers import aead
@@ -12,9 +12,9 @@ def encrypt(message: bytes, key: bytes) -> bytes:
     # cape key is DEM-encoded RSA key
     rsa_key = _parse_rsa_key(key)
     # create ephemeral AES key
-    aes_key = _aes_keygen(32)
+    aes_key = _aes_keygen(256)
     # encrypt message w/ AES
-    data_ctxt, nonce = _aes_encrypt(message)
+    data_ctxt, nonce = _aes_encrypt(message, aes_key)
     # encrypt the ephemeral AES key w/ RSA
     key_ctxt = _rsa_encrypt(aes_key, rsa_key)
     # concatenate everything into a single bytes obj
@@ -30,11 +30,16 @@ def _aes_encrypt(inputs: bytes, key: bytes) -> Tuple[bytes, bytes]:
 
 
 def _aes_keygen(bitlength: int) -> bytes:
-    return aead.AESGCM.generate_aes_key(bitlength)
+    return aead.AESGCM.generate_key(bitlength)
 
 
 def _parse_rsa_key(key: bytes) -> rsa.RSAPublicKey:
-    return x509.load_der_x609_certificate(key).public_key()
+    public_key = serialization.load_der_public_key(key)
+    if not isinstance(public_key, rsa.RSAPublicKey):
+        raise ValueError(
+            f"Decoded 'key' expected to be RSAPublicKey, found {type(public_key)}"
+        )
+    return public_key
 
 
 def _rsa_encrypt(inputs: bytes, public_key: rsa.RSAPublicKey) -> bytes:
