60e31f0e4f237274f87d785150b8f0ab3d13db3f
Yann Dupis <yann.dupis@gmail.com>
2022-09-20 12:17:31 -0700

    Add cape deploy & token from cli (#92)

--- examples/deploy_run_echo.py
+++ examples/deploy_run_echo.py
@@ -1,38 +1,16 @@
 import pathlib
-import subprocess
 
 import pycape
+from pycape.experimental import cli
 
 CAPE_HOST = "wss://enclave.capeprivacy.com"
 ECHO_DEPLOY_PATH = pathlib.Path(__file__).parent.absolute() / "echo"
 
-# Cape deploy function
-proc_deploy = subprocess.Popen(
-    f"cape deploy {ECHO_DEPLOY_PATH} --url {CAPE_HOST}",
-    shell=True,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
-out_deploy, err_deploy = proc_deploy.communicate()
-err_deploy = err_deploy.decode()
 
-# Parse stderr to get function id & function checksum
-err_deploy = err_deploy.split("\n")
-function_id = function_checksum = None
-for i in err_deploy:
-    if "Function ID" in i:
-        id_output = i.split(" ")
-        function_id = id_output[3]
-    elif "Checksum" in i:
-        checksum_output = i.split(" ")
-        function_checksum = checksum_output[2]
-
-if function_id is None:
-    raise RuntimeError(f"Function ID not found in 'deploy' response: \n{err_deploy}")
-
-# Run function
-function_ref = pycape.FunctionRef(function_id, function_checksum)
 cape = pycape.Cape(url=CAPE_HOST)
+# Deploy Cape function
+function_ref = cli.deploy(ECHO_DEPLOY_PATH)
 message = cape.encrypt("Welcome to Cape".encode())
+# Run Cape function
 result = cape.run(function_ref, message)
 print(f"The result is: {result.decode()}")
