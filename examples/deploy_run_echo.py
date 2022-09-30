e6b5f87d3471fe9628a1ce32a43d95e793a8d7be
yanndupis <yann.dupis@gmail.com>
2022-09-30 13:53:03 -0700

    Add comment

--- examples/deploy_run_echo.py
+++ examples/deploy_run_echo.py
@@ -10,6 +10,7 @@ ECHO_DEPLOY_PATH = pathlib.Path(__file__).parent.absolute() / "echo"
 cape = pycape.Cape(url=CAPE_HOST)
 # Deploy Cape function
 function_ref = cli.deploy(ECHO_DEPLOY_PATH, token_expiry=100)
+# Encrypt input
 message = cape.encrypt("Welcome to Cape".encode(), function_ref.token)
 # Run Cape function
 result = cape.run(function_ref, message)
