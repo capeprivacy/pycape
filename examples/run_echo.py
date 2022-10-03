032724cd52872607c47b8b633640bda484359017
Yann Dupis <yann.dupis@gmail.com>
2022-10-03 13:29:05 -0700

    Rename token file to function_json in run_echo ex
    Co-authored-by: jvmncs <jvmncs@gmail.com>

--- examples/run_echo.py
+++ examples/run_echo.py
@@ -6,8 +6,8 @@ from pycape import FunctionRef
 
 if __name__ == "__main__":
     url = os.environ.get("CAPE_HOST", "wss://enclave.capeprivacy.com")
-    token_file = os.environ.get("TOKEN_FILE", "echo_token.json")
-    token_file = pathlib.Path(__file__).parent.absolute() / token_file
+    function_json = os.environ.get("FUNCTION_JSON", "echo_token.json")
+    function_json = pathlib.Path(__file__).parent.absolute() / function_json
 
     function_ref = FunctionRef.from_json(token_file)
     cape = Cape(url=url)
