9a0d330022f6a61e6250da78fedbca295a49a45c
ChrisFriesen <26197502+ChrisFriesen@users.noreply.github.com>
2022-12-05 20:19:00 -0800

    Adjusting bump2version config

--- cape_encrypt/cape_encrypt/__init__.py
+++ cape_encrypt/cape_encrypt/__init__.py
@@ -1,7 +1,8 @@
-from cape_encrypt.cape_encrypt import encrypt
 from cape_encrypt.cape_encrypt import decrypt
+from cape_encrypt.cape_encrypt import encrypt
 
-__version__ = "0.1.0-rc"
+__version__ = "0.1.0"
+
 __all__ = [
     "encrypt",
     "decrypt",
