0bc8fb442bcf64f3a3fe0d65a04e0bf5849e5065
jvmncs <jason@capeprivacy.com>
2023-02-02 11:10:40 -0500

    refactor cape key/encrypt to accept username parameter (#111)
    * refactor cape key/encrypt to accept username parameter
    
    * add user_encrypt example
    
    * keyword-only args to cape.key and cape.encrypt
    
    * lint

--- examples/deploy_run_echo.py
+++ examples/deploy_run_echo.py
@@ -10,8 +10,12 @@ ECHO_DEPLOY_PATH = pathlib.Path(__file__).parent.absolute() / "echo"
 cape = pycape.Cape(url=CAPE_HOST)
 # Deploy Cape function
 function_ref = cli.deploy(ECHO_DEPLOY_PATH, token_expiry=100)
+print("Echo deployed:")
+print(f"\t- ID: {function_ref.id}")
+print(f"\t- Token: {function_ref.token}")
+print(f"\t- Checksum: {function_ref.checksum}")
 # Encrypt input
-message = cape.encrypt("Welcome to Cape".encode(), function_ref.token)
+message = cape.encrypt("Welcome to Cape".encode(), token=function_ref.token)
 # Run Cape function
 result = cape.run(function_ref, message)
 print(f"The result is: {result.decode()}")
