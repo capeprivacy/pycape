e4a36de343d4074da02b9c713b92077c4bed5c38
Justin Patriquin <justinpatriquin@gmail.com>
2023-09-26 13:26:37 -0300

    lint

--- examples/llama/llama.py
+++ examples/llama/llama.py
@@ -1,5 +1,6 @@
-from pycape.llms import Cape
 import os
+
+from pycape.llms import Cape
 
 c = Cape(url="https://api.capeprivacy.com")
 
@@ -7,7 +8,11 @@ token = c.token(os.getenv("CAPE_TOKEN", ""))
 
 for msg in c.chat_completions(
     [
-        {"role": "user", "content": "<s>[INST] <<SYS>>You are a helpful Assistant.<</SYS>>\n\nWhat is the Capital of France? [/INST]"},
+        {
+            "role": "user",
+            "content": "<s>[INST] <<SYS>>You are a helpful Assistant.<</SYS>>"
+            "\n\nWhat is the Capital of France? [/INST]",
+        },
         {"role": "system", "content": "you are a happy helpful assistant"},
     ],
     token,
